cd19f2c923ad4df5d37d7f5bbe5a0000eae63c45
香楠 <zhangqixcu@gmail.com>
2022-10-10 19:36:30 +0800

    first commit

---
+++ blog-service/main.go
@@ -0,0 +1,100 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"github.com/natefinch/lumberjack"
+	"global"
+	"log"
+	"logger"
+	"net/http"
+	"routers"
+	"senyas.tour.com/blog-service/internal/model"
+	"setting"
+	"time"
+)
+
+func init() {
+	err := setupSetting()
+	if err != nil {
+		log.Fatalf("init.setupSetting err: %v", err)
+	}
+
+	err = setupLogger()
+	if err != nil {
+		log.Fatalf("init.setupLogger err: %v", err)
+	}
+
+	err = setupDBEngine()
+	if err != nil {
+		log.Fatalf("init.setupDBEngine err: %v", err)
+	}
+}
+
+func main() {
+
+	fmt.Println(global.ServerSetting)
+	fmt.Println(global.AppSetting)
+	fmt.Println(global.DatabaseSetting)
+
+	global.Logger.Infof("%s: senyas.tour/%s", "eddycjy", "blog-service")
+	global.Logger.Info("%s: senyas.tour/%s", "eddycjy", "blog-service")
+
+	gin.SetMode(global.ServerSetting.RunMode)
+	router := routers.NewRouter()
+	s := &http.Server{
+		Addr:           ":" + global.ServerSetting.HttpPort,
+		Handler:        router,
+		ReadTimeout:    global.ServerSetting.ReadTimeout,
+		WriteTimeout:   global.ServerSetting.WriteTimeout,
+		MaxHeaderBytes: 1 << 20,
+	}
+	//_ = s.ListenAndServe()
+	if err := s.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+		fmt.Println(err)
+	}
+}
+
+func setupSetting() error {
+	newSetting, err := setting.NewSetting()
+	if err != nil {
+		return err
+	}
+	err = newSetting.ReadSection("Server", &global.ServerSetting)
+	if err != nil {
+		return err
+	}
+	err = newSetting.ReadSection("App", &global.AppSetting)
+	if err != nil {
+		return err
+	}
+	err = newSetting.ReadSection("Database", &global.DatabaseSetting)
+	if err != nil {
+		return err
+	}
+
+	global.ServerSetting.ReadTimeout *= time.Second
+	global.ServerSetting.WriteTimeout *= time.Second
+	return nil
+}
+
+func setupLogger() error {
+	global.Logger = logger.NewLogger(&lumberjack.Logger{
+		Filename:  global.AppSetting.LogSavePath + "/" + global.AppSetting.LogFileName + global.AppSetting.LogFileExt,
+		MaxSize:   600,
+		MaxAge:    10,
+		LocalTime: true,
+	}, "", log.LstdFlags).WithCaller(2)
+
+	return nil
+}
+
+func setupDBEngine() error {
+	var err error
+	global.DBEngine, err = model.NewDBEngine(global.DatabaseSetting)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
